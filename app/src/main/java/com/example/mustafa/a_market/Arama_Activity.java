5fa7c4224654a24d0308d643672c0ef49032e23d
Mustafa Tayiz <m.mustafa.tayiz@gmail.com>
2018-03-22 18:34:37 +0300

    Bulunan ürünlerin görüntülenmesi için listview eklendi.

--- app/src/main/java/com/example/mustafa/a_market/Arama_Activity.java
+++ app/src/main/java/com/example/mustafa/a_market/Arama_Activity.java
@@ -6,23 +6,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.*;
 
+import java.util.List;
+
 /**
  Created by mustafa on 04.03.2018.
  */
 
 public class Arama_Activity extends AppCompatActivity {
 
-    // component tanımlama
+    //--- component tanımlama
     Button barkodBtn;
-    //
+    ListView bulunanUrunlerLV;
+    //---
+
+    String[] gelenDizi = new String[10];
 
 
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arama_s);
+        //--- nesneyi xml ile bağlama
+        barkodBtn = (Button) findViewById(R.id.barkodBtn);
+        //----
+
+        //--- List view işleri
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            gelenDizi[i]= "deneme" + i;
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, gelenDizi);
+
+        bulunanUrunlerLV = (ListView) findViewById(R.id.bulunanUrunlerLV);
+        bulunanUrunlerLV.setAdapter(adapter);
+
+        //---
 
         //Barcode okuma Sayfası Açıldı(Ramazan)
-            barkodBtn = (Button) findViewById(R.id.barkodBtn);
+
             barkodBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,13 +54,8 @@ public class Arama_Activity extends AppCompatActivity {
                 Intent intent=new Intent(Arama_Activity.this,barcodeShow.class);
                 startActivity(intent);
 
-
-
             }
         });
 
     }
-
-
-
 }
